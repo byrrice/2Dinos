c41a29ca189b3531d2cfd2a3c193f7b813c8fd7a
byrrice <32532726+byrrice@users.noreply.github.com>
2018-06-26 23:34:33 -0600

    level 2 cutscenes

---
+++ Assets/Scripts/CutsceneDialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneDialogue : MonoBehaviour {
+
+	[SerializeField] Canvas messageCanvas;
+	public float messageTime = 0f;
+	public float delay = 0f;
+
+	// Use this for initialization
+	void Start () {
+		messageCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//GOT CUTSCENES WORKING THANK GOD HOLY CAPTIALIZE AN O
+	//Need to set up a sprite to tell us that it is a hint/dialogue system
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.tag == "Player"){
+			StartCoroutine(SendMessage());
+		}
+	}
+	public IEnumerator SendMessage() {
+		yield return new WaitForSeconds(delay);
+		messageCanvas.enabled = true;
+		yield return new WaitForSeconds(messageTime); // waits for message time
+		messageCanvas.enabled = false;
+ 	}
+}
